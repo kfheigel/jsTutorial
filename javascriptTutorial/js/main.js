6901063062aa32b27665027ce9373d797fca63f8
kheigel <kfheigel@gmail.com>
2024-04-10 09:07:48 +0200

    classes

--- javascriptTutorial/js/main.js
+++ javascriptTutorial/js/main.js
@@ -1,36 +1,23 @@
-let playGame = confirm ("shall we play?");
-
-if (playGame) {
-    let playerChoice = prompt("please enter rock, paper or scissors.")
-    if (playerChoice) {
-        let playerOne = playerChoice.trim().toLowerCase();
-        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
-            let computerChoice = Math.floor(Math.random() * 3 + 1)
-            let computer = computerChoice === 1 ? "rock"
-                : computerChoice === 2 ? "paper"
-                : "scissors";
-
-                let result = 
-                    playerOne === computer ? "Tie game"
-                    : playerOne === "rock" && computer === "paper"
-                    ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                    : playerOne === "paper" && computer === "scissors"
-                    ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                    : playerOne === "scissors" && computer === "rock"
-                    ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                    : `playerOne: ${playerOne}\nComputer: ${computer} \nPlayer wins!`;
-
-                alert(result)
-
-                let playAgain = confirm("play again?");
-                playAgain ? location.reload() : alert("Ok, thanks for playing");
-        } else {    
-            alert("You didnt't enter rock, paper, scissors");
-        }
-
-    } else {
-        alert ("I guess you changed your mind.");
+class Pizza {
+    constructor (pizzaType, pizzaSize = "medium") {
+        this.pizzaType = pizzaType;
+        this.size = pizzaSize;
+        this.crust = "original"
     }
-} else {
-    alert("Ok, maybe next time");
+
+    getCrust() {
+        return this.pizzaCrust;
+    }
+
+    setCrust(crust) {
+        this.crust = crust; 
+    }
+
+    bake() {
+        console.log(`Baking a ${this.size} ${this.pizzaType} ${this.crust} crust pizza`)
+    }
 }
+const myPizza = new Pizza("pepperoni", "small");
+
+myPizza.setCrust("thin");
+myPizza.bake();
